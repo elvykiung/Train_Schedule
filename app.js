d9cdd5b68a7e93839cdfbeaefdf15c78145b7126
Elvy Kiung <kiungh22@gmail.com>
2019-05-11 01:10:25 -0700

    set up database

--- app.js
+++ app.js
@@ -23,3 +23,63 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
 var database = firebase.database();
+
+//=================================================
+
+//2. when click, add the input data to database and clar the inputbox
+$('#add-train-btn').on('click', function(event) {
+  event.preventDefault();
+
+  //Grabs the user input
+
+  var trainName = $('#train-name-input')
+    .val()
+    .trim();
+  var destination = $('#destination-input')
+    .val()
+    .trim();
+  var trainStart = $('#first-train-time-input')
+    .val()
+    .trim();
+
+  var frequency = $('#frequency-input')
+    .val()
+    .trim();
+
+  var newTrain = {
+    TrainName: trainName,
+    Destination: destination,
+    FirstTrain: trainStart,
+    Frequency: frequency
+  };
+
+  //uploads the data to database
+  database.ref().push(newTrain);
+  console.log('==========================');
+  console.log(newTrain.TrainName);
+  console.log(newTrain.Destination);
+  console.log(newTrain.FirstTrain);
+  console.log(newTrain.Frequency);
+
+  //clears inout box
+  $('#train-name-input').val('');
+  $('#destination-input').val('');
+  $('#first-train-time-input').val('');
+  $('#frequency-input').val('');
+});
+// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+
+database.ref().on('child_added', function(childSnapshot) {
+  console.log('=======================');
+  console.log(childSnapshot.val());
+
+  var trainName = childSnapshot.val().TrainName;
+  var destination = childSnapshot.val().Destination;
+  var firstTrain = childSnapshot.val().FirstTrain;
+  var frequency = childSnapshot.val().Frequency;
+  console.log('========================');
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrain);
+  console.log(frequency);
+});
